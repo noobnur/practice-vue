a71cdc73e947f2a281003e780b2a1a7ef16106df
noobnur <nurhsans@gmail.com>
2019-01-26 15:50:58 +0800

    more practice

--- js/index.js
+++ js/index.js
@@ -1,3 +1,56 @@
+Vue.component('todo-item', {
+  // The todo-item component now accepts a
+  // "prop", which is like a custom attribute.
+  // This prop is called todo.
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
+
+var plain = new Vue({
+  el: '#try',
+  data: {
+    rawHtml: '<span style="color:red">This should be red</span>'
+  }
+})
+
+
+var vm = new Vue({
+  el: '#example',
+  data: {
+    message: 'Hello'
+  },
+  computed: {
+    // a computed getter
+    reversedMessage: function () {
+      // `this` points to the vm instance
+      return this.message.split('').reverse().join('')
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      return this.message.split('').reverse().join('')
+    }
+  }
+})
+
+
+
 new Vue({
     el: '#app',
     data: {
@@ -14,5 +67,12 @@ new Vue({
       reverseMessage: function () {
         this.message = this.message.split('').reverse().join('')
       }
+    }
+  })
+
+  var app6 = new Vue({
+    el: '#app-6',
+    data: {
+      string: 'Change my data!'
     }
   })
